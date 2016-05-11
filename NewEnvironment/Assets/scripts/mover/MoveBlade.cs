eabdd00b189ba799040f54bc0ede5144dd2a8fa7
Pallab Mahmud <pmahmud@uoregon.edu>
2016-05-10 21:04:12 -0700

    fix local position problem

--- NewEnvironment/Assets/scripts/mover/MoveBlade.cs
+++ NewEnvironment/Assets/scripts/mover/MoveBlade.cs
@@ -13,17 +13,17 @@ public class MoveBlade : MonoBehaviour {
 
 	void Start () {
         if (alongX){
-            pos1 = new Vector3(minVal, transform.position.y, 0f);
-            pos2 = new Vector3(maxVal, transform.position.y, 0f);
+            pos1 = new Vector3(minVal, transform.localPosition.y, 0f);
+            pos2 = new Vector3(maxVal, transform.localPosition.y, 0f);
         }
         else if (alongY){
-            pos1 = new Vector3(transform.position.x, minVal, 0f);
-            pos2 = new Vector3(transform.position.x, maxVal, 0f);
+            pos1 = new Vector3(transform.localPosition.x, minVal, 0f);
+            pos2 = new Vector3(transform.localPosition.x, maxVal, 0f);
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+        transform.localPosition = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
 	}
 }
