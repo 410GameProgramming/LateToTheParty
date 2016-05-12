c0ceebe405546c1e1f04eda8ca890fecb9c2c603
Pallab Mahmud <pmahmud@uoregon.edu>
2016-05-11 17:48:12 -0700

    pickup script added

---
+++ NewEnvironment/Assets/scripts/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    public string type;
+
+    private PlayerController player;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if(col.tag == "Player")
+        {
+            GetComponentInParent<PlayerController>();
+            //player.pickup(type);
+        }
+
+    }
+}
