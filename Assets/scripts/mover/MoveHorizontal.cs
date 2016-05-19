891921bd73010e893fe1d0ea8326a6c5934a4913
Pallab Mahmud <pmahmud@uoregon.edu>
2016-05-18 20:51:17 -0700

    more mover scripts

---
+++ Assets/scripts/mover/MoveHorizontal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveHorizontal : MonoBehaviour {
+
+    public int index;
+    private Vector3 pos1;
+    private Vector3 pos2;
+    public float minX;
+    public float maxX;
+    public float speed;
+
+	void Start () {
+        //Vector2[] blockPositions = GameManager.instance.getBlockPositions();
+        pos1 = new Vector3(minX, transform.position.y, 0f);
+        pos2 = new Vector3(maxX, transform.position.y, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+	}
+}
---
+++ Assets/scripts/mover/MoveVertical.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveVertical : MonoBehaviour {
+
+    public int index;
+    private Vector3 pos1;
+    private Vector3 pos2;
+    public float minY;
+    public float maxY;
+    public float speed;
+
+	void Start () {
+        //Vector2[] blockPositions = GameManager.instance.getBlockPositions();
+        pos1 = new Vector3(transform.position.x, minY, 0f);
+        pos2 = new Vector3(transform.position.x, maxY, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+	}
+}
