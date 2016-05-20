6639cc62c8b10a34d414d536c8185f7c00cae50d
Segejin <craigt@uoregon.edu>
2016-05-19 23:48:00 -0700

    Score display on level 0

---
+++ Assets/scripts/ui/Score.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    private Text score;
+
+    void Start() {
+        score = gameObject.GetComponent<Text>();
+    }
+    void Update() {
+        score.text = GameManager.instance.totalScore.ToString();
+    }
+}
