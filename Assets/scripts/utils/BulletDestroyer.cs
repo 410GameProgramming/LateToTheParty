ecef32efa40e816e65a786778d5cf5dff8eb51a9
Pallab Mahmud <pmahmud@uoregon.edu>
2016-05-24 21:58:54 -0700

    bullet destroyer

---
+++ Assets/scripts/utils/BulletDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDestroyer : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Projectile")
+        {
+            print("found");
+            Destroy(gameObject);
+        }
+    }
+}
