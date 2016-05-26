673c0ab9645018cbb805835e9e175112ee5e3cd0
Pallab Mahmud <pmahmud@uoregon.edu>
2016-05-26 13:34:44 -0700

    adding screen fader

---
+++ Assets/ScreenFader/ScreenFadeControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ScreenFadeControl : MonoBehaviour
+{
+    public Material fadeMaterial = null;
+
+    // Based on OVRScreenFade
+#if UNITY_ANDROID && !UNITY_EDITOR
+	void OnCustomPostRender()
+#else
+    void OnPostRender()
+#endif
+    {
+        fadeMaterial.SetPass(0);
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        GL.Color(fadeMaterial.color);
+        GL.Begin(GL.QUADS);
+        GL.Vertex3(0f, 0f, -12f);
+        GL.Vertex3(0f, 1f, -12f);
+        GL.Vertex3(1f, 1f, -12f);
+        GL.Vertex3(1f, 0f, -12f);
+        GL.End();
+        GL.PopMatrix();
+    }
+}
