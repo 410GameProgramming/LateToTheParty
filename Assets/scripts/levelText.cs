83ea0596c7811a663398728ede3d09a5cefe2807
Pallab Mahmud <pmahmud@uoregon.edu>
2016-06-05 22:06:24 -0700

    Level Text added

---
+++ Assets/scripts/levelText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class levelText : MonoBehaviour {
+
+	// Use this for initialization
+    //Text levelText;
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = "level " + GameManager.instance.currentLevel;
+	}
+}
